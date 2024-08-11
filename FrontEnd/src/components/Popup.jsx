ddb13ed47db5b48678d1773abcbb1ff14340765c
Deep_Lajpal <lajpaldeep@gmail.com>
2024-08-12 00:21:50 +0530

    chore: Add timer functionality to Popup component

--- FrontEnd/src/components/Popup.jsx
+++ FrontEnd/src/components/Popup.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import styles from "../styles/Popup.module.scss"
 import { RxCross2 } from "react-icons/rx";
 
 const Popup = () => {
+    const [timerMinutes, setTimerMinutes] = useState(0)
+    const [timerSeconds, setTimerSeconds] = useState(30)
     return (
         <div className={styles.popupContainer}>
             <div className={styles.popupContent}>
@@ -10,8 +13,11 @@ const Popup = () => {
                         <p className={styles.closeBtn}><RxCross2 /></p>
                     </div>
                     <div className={styles.closeTimerContainer}>
-                        <p className={styles.closeTimer}>0.30</p>
+                        <p className={styles.closeTimer}>{timerMinutes}:{timerSeconds}</p>
                     </div>
+                </div>
+                <div className={styles.popupMiddleContainer}>
+                    
                 </div>
             </div>
         </div>
